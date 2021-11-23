bcc986f7b38c1592e9378bbd8ba4cd0c1ea5cd49
minhpn <phamngocminh7694@gmail.com>
2021-11-23 23:26:51 +0700

    change line

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import CommentBox from "../CommentBox";
 import App from "../App";
 
+
 it("should be show box comment", () => {
   const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
